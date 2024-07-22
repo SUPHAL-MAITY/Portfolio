c7bf8102653de0af3adf5198d1fec257ce2910b5
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-07-22 20:04:45 +0530

    services page mobile view

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const Services = () => {
   return (
-    <div className="p-16 grid  grid-cols-3">
+    <div className="p-16 max-[640px]:flex flex-col  grid  grid-cols-3">
 
 
 
 
 
-      <div className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+      <div className="relative  flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
         <div className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
           <Image
             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
