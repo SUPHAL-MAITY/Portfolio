5545c6e221383bf1f005d9dab1ba7d272340cc77
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-03-03 10:40:53 +0530

    change of cv

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -51,14 +51,14 @@ const Contact = () => {
         </div>
 
         <div className="flex justify-center p-2 font-serif">
-          <a href="/suphal___maity.pdf" download="suphal___maity" >
+          <a href="/Suphal_maity_MERN.pdf" download="suphal___maity" >
           <button
             type="button"
             className="max-w-2xl  inline-block rounded-2xl bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-2xl transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
           >
             Download CV
           </button>
-
+          
           </a>
           
         </div>
